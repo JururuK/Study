8f29c87e946d6ea224e9cdd5ae6677e1342f59e3
JururuK <86296317+JururuK@users.noreply.github.com>
2021-06-28 22:04:13 +0900

    Add files via upload
    baekjoon- python exercise

---
+++ 6.28.py
@@ -0,0 +1,131 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "id": "757195a6-caa9-456d-9785-e00c08e62cd7",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "강한친구 대한육군\n",
+      "강한친구 대한육군\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(\"강한친구 대한육군\",\"강한친구 대한육군\", sep = '\\n')"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 7,
+   "id": "b3149879-6cf0-4246-a9c4-d4300a2cd652",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "\\    /\\ \n",
+      " )  ( ')\n",
+      "(  /  )\n",
+      " \\(__)|\n"
+     ]
+    }
+   ],
+   "source": [
+    "a=\"\\    /\\ \"\n",
+    "b=\" )  ( ')\"\n",
+    "c=\"(  /  )\"\n",
+    "d=\" \\(__)|\"\n",
+    "\n",
+    "print(a,b,c,d,sep='\\n')"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 15,
+   "id": "20528484-c928-4c40-be43-b91e531711b2",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "|\\_/|\n",
+      "|q p|   /}\n",
+      "( 0 )\"\"\"\\ \n",
+      "|\"^\"`    |\n",
+      "||_/=\\\\__|\n"
+     ]
+    }
+   ],
+   "source": [
+    "a=\"|\\_/|\"\n",
+    "b=\"|q p|   /}\"\n",
+    "c='( 0 )\"\"\"\\ '\n",
+    "d='|\"^\"`    |'\n",
+    "e='||_/=\\\\\\__|'\n",
+    "print(a,b,c,d,e,sep='\\n')"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 18,
+   "id": "4ff71580-1d3d-4a0a-aecd-3db8130887ea",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      " 1 2\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "3\n"
+     ]
+    }
+   ],
+   "source": [
+    "A,B = map(int, input().split())\n",
+    "print(A+B)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "1ebdf302-9138-4606-8e82-ef224a89d4ae",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.10"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
